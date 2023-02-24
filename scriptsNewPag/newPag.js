2ea16c8a167c6708a6acbbcbb63858be792db100
Davichin21 <superlokitoxd@gmail.com>
2023-02-23 22:24:08 -0500

    publico

--- scriptsNewPag/newPag.js
+++ scriptsNewPag/newPag.js
@@ -24,21 +24,40 @@ scrollables.forEach(scrollable => {
 //------------------------------------- Boton 3 -------------------------------------------//
 if (true) {
 button = document.getElementById('button-3')
-
+container = document.getElementById('container');
 body = document.getElementById('body');
 
 var contador = 0;
 button.onclick = function () {
-if (contador%2 == 0) {
-body.style.backgroundColor= '#242a30';
+if (contador%2 == 0) {   //primer cambio del boton
+  container.style.maxWidth = "100%";
     
 }
-else if(contador%2 != 0){
-    body.style.backgroundColor= '#0d0f12';
+else if(contador%2 != 0){// segundo cambio del boton
+  container.style.maxWidth = "100%";
     
 }
     contador++
     console.log(contador)
+}
+}
+if (true) {
+  const iframe = document.querySelectorAll('.iframe');
+
+if (container.style.maxWidth == "100%") {
+  for (let i = 0; i < iframe.length; i++) {
+    iframe[i].style.width = '400px';
+  }
+  
+}
+else if(  container.style.maxWidth == "1200px"){
+  for (let i = 0; i < iframe.length; i++) {
+    iframe[i].style.width = '450px';
+  }
+  
+}
+else{
+
 }
 }
 //-----------------------------------------------------------------------------------------//
@@ -74,3 +93,36 @@ input.addEventListener("keydown", function(event) {
     cambiarTexto();
   }
 });
+//----------------------------------------------------------------------------------------------//
+
+
+//------------------------------------ Creandor de espacios ------------------------------------//
+var buttonAdd = document.getElementById("buttonAdd");
+var container = document.getElementById("container");
+var ndiv = 0;
+function iframeAdd(){
+  ++ndiv;
+  document.getElementById("container").innerHTML += `
+  <div id="inputs">
+  <ul id="ul-inputs">  
+      <li class="li">
+          <input type="text" placeholder="Enter url" id="inputUrl" pattern="^https://.*" title="Por favor, introduce un enlace que comience con https://">
+          <button onclick="cambiarTexto()" id="bubuton">→</button>
+          <div class="item">   
+              <a id="aa" >Otras</a>
+              <div class="menu">       
+                  <ul>
+                      <li><a >Opcion 1</a></li>
+                      <li><a >Opcion 2</a></li>
+                  </ul>
+              </div>
+          </div> 
+          <p id="p${ndiv}" ></p>
+      </li>  
+  </ul>
+      <iframe src="index.html" title="Iframe Example" ></iframe>
+</div>`
+}
+
+
+//----------------------------------------------------------------------------------------------//
